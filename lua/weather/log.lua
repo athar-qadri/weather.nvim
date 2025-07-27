be23ceec79226b4855e6410715768510d7b426ca
Athar Qadri <atharr.qadri@gmail.com>
2025-07-27 22:01:27 +0530

    refactor: plugin name

--- lua/weather/log.lua
+++ lua/weather/log.lua
@@ -9,7 +9,7 @@
 -- User configuration section
 local default_config = {
 	-- Name of the plugin. Prepended to log messages
-	plugin = "scratchpad.nvim",
+	plugin = "weather.nvim",
 
 	-- Should print the output to neovim while running
 	use_console = true,
